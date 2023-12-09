e065bc4ddb4b50dea5d1127819cc087181232d48
Abhimanyu <iamabhimanyu08@gmail.com>
2023-12-09 18:39:55 +0530

    maximum of a list using a function

--- maximum_function.s
+++ maximum_function.s
@@ -1,7 +1,8 @@
+
 .section .data
 
 list:
-.long 12,20,10,3,4,7,0
+.long 12,20,10,30,4,7,0
 
 .section .text
 
@@ -15,7 +16,11 @@ pushl $7
 pushl $list
 call max_list
 
+addl $8, %esp
 
+jmp end
+
+end:
 movl %eax, %ebx
 movl $1, %eax
 int $0x80
@@ -31,7 +36,7 @@ max_list:
 	movl %esp, %ebp #point new base pointer to top of stack
 	subl $4, %esp #local variable which'll store current max value
 
-	movl $0, %edi #%edi will store the current index
+	movl $-4, %edi #%edi will store the current index
 	movl 8 (%ebp), %ebx #address to the start of the list
 	movl (%ebx), %eax
 	movl %eax, -4 (%ebp) #local variable stores max till now which is the first element in list at the start
@@ -41,17 +46,19 @@ max_list:
 	start_loop:
 		movl 12 (%ebp), %ebx  #move length of list to %ebx
 		imull $4, %ebx #multiply length by 4
+		addl $4, %edi
 		cmpl %ebx, %edi #if equal end
 		je loop_end
 
 		
 		movl 8 (%ebp), %ebx #move address to start of list into %ebx
-		movl %edi (%ebx), %eax #move first item to %eax
-		cmpl %eax, -4 (%ebp)
-		jge start_loop
+		addl %edi, %ebx
+		movl (%ebx), %eax #move current item to %eax
+		cmpl -4 (%ebp), %eax
+		jle start_loop
+
 
 		movl %eax, -4 (%ebp)
-		addl $4, %edi
 		jmp start_loop
 
 	loop_end:
@@ -61,7 +68,8 @@ max_list:
 		ret
 
 
-
-
+end_test:
+	movl $1, %eax
+	int $0x80
 
 
